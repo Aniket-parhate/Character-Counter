3a064780739c15f5d15dd1455c8efd81c8df7cb5
Aniket-parhate <parateaniket2008@gmail.com>
2022-11-29 08:01:50 +0530

    added remaining functionality

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 var tweet = prompt("Compose your Tweet");
 var tweetCount = tweet.length;
-alert("You have written " + tweetCount + " characters");
+alert(
+  "You have written " +
+    tweetCount +
+    " characters You have " +
+    (100 - tweetCount) +
+    " characters remaining"
+);
